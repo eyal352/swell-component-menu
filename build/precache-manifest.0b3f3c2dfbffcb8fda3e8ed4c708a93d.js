dfb7a1656f7dfb9fa62a12ad2c610fe1e91197dc
Eyal C <eyal.chistik@gmail.com>
2020-01-22 17:54:25 +0200

    Updates from Master

---
+++ build/precache-manifest.0b3f3c2dfbffcb8fda3e8ed4c708a93d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362361bf800370553d32406a82b5b651",
+    "url": "/swell-component-menu/index.html"
+  },
+  {
+    "revision": "0408015cc89040b8cc26",
+    "url": "/swell-component-menu/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a86df155636048b13854",
+    "url": "/swell-component-menu/static/css/main.4fa4bb72.chunk.css"
+  },
+  {
+    "revision": "0408015cc89040b8cc26",
+    "url": "/swell-component-menu/static/js/2.71e4edc7.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/swell-component-menu/static/js/2.71e4edc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a86df155636048b13854",
+    "url": "/swell-component-menu/static/js/main.675d0186.chunk.js"
+  },
+  {
+    "revision": "ba30bc2337b3a67420ec",
+    "url": "/swell-component-menu/static/js/runtime-main.4d741895.js"
+  }
+]);
